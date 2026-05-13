ce1ee296948f4eb933bb1f518385eff4f81f04bc
A6P3W <186453117+A6P3W@users.noreply.github.com>
2026-05-14 00:43:19 +0900

    [Feature] MMovementComponentの追加とプロジェクトへの組み込み
    MMovementComponentクラスを新規作成し、2D速度ベクトルによる移動と減衰処理（OnUpdate, AddForce）を実装。関連ヘッダー・ソースをプロジェクトファイルに追加。

---
+++ Engine/EngineCore/Components/Private/MMovementComponent.cpp
@@ -0,0 +1,15 @@
+﻿#include "MMovementComponent.h"
+#include <SceneComponent.h>
+#include <Actor.h>
+#include <Utils/UMath.h>
+void MMovementComponent::AddForce(const FVector2D& Force)
+{
+}
+
+void MMovementComponent::OnUpdate(float DeltaTime) 
+{
+	if (!GetOwner())return;
+	AActor* owner = GetOwner();
+	Velocity *= 0.96;
+	owner->AddActorWorldOffset(Velocity);
+}
---
+++ Engine/EngineCore/Components/Public/MMovementComponent.h
@@ -0,0 +1,14 @@
+﻿#pragma once
+#include "Utils/UMath.h"
+#include "ActorComponent.h"
+class MMovementComponent : public MActorComponent
+{
+public:
+	void AddForce(const FVector2D& Force);
+
+	void OnUpdate(float DeltaTime) override;
+private:
+	FVector2D Velocity;
+	float Friction = 0.98f;
+};
+
