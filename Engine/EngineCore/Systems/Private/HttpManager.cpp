ff0f2d63ea10fb4a4e6c33a26d1a097746f50b25
A6P3W <186453117+A6P3W@users.noreply.github.com>
2026-06-15 00:34:40 +0900

    feat: HttpManagerの追加およびcpr/libcurl静的リンクのビルドエラー解消
    ・cprを用いた通信を行うHttpManagerの実装と登録
    
    ・vcpkgのマニフェスト有効化、x64-windows-static-mdへの変更
    
    ・不足していたWindowsシステムライブラリのリンク指定とパス解決の改善

---
+++ Engine/EngineCore/Systems/Private/HttpManager.cpp
@@ -0,0 +1,86 @@
+﻿#include "HttpManager.h"
+#include <cpr/cpr.h>
+#include <thread>
+#include <mutex>
+#include <vector>
+
+struct HttpResult {
+    HttpResponse response;
+    HttpCallback callback;
+};
+
+struct HttpManager::Impl {
+    std::mutex queueMutex;
+    std::vector<HttpResult> resultQueue;
+};
+
+HttpManager& HttpManager::GetInstance() {
+    static HttpManager instance;
+    return instance;
+}
+
+HttpManager::HttpManager() {
+    m_Impl = new Impl();
+}
+
+HttpManager::~HttpManager() {
+    delete m_Impl;
+}
+
+void HttpManager::Update() {
+    std::vector<HttpResult> resultsToProcess;
+
+    // ロックを掛けて、現在のキューをすべて取り出す
+    {
+        std::lock_guard<std::mutex> lock(m_Impl->queueMutex);
+        if (!m_Impl->resultQueue.empty()) {
+            resultsToProcess = std::move(m_Impl->resultQueue);
+            m_Impl->resultQueue.clear();
+        }
+    }
+
+    // メインスレッドでコールバックを実行
+    for (auto& res : resultsToProcess) {
+        if (res.callback) {
+            res.callback(res.response);
+        }
+    }
+}
+
+void HttpManager::Get(const std::string& url, HttpCallback callback) {
+    // スレッドを分離して裏で通信を実行
+    std::thread([this, url, callback]() {
+        cpr::Response r = cpr::Get(cpr::Url{ url });
+
+        HttpResponse response;
+        response.StatusCode = r.status_code;
+        response.Body = r.text;
+        response.ErrorMessage = r.error.message;
+        response.bSuccess = (r.status_code >= 200 && r.status_code < 300);
+
+        // 結果をスレッドセーフにキューに追加
+        std::lock_guard<std::mutex> lock(m_Impl->queueMutex);
+        m_Impl->resultQueue.push_back({ std::move(response), std::move(callback) });
+
+        }).detach();
+}
+
+void HttpManager::PostJson(const std::string& url, const std::string& jsonBody, HttpCallback callback) {
+    std::thread([this, url, jsonBody, callback]() {
+        cpr::Response r = cpr::Post(
+            cpr::Url{ url },
+            cpr::Body{ jsonBody },
+            cpr::Header{ {"Content-Type", "application/json"} }
+        );
+
+        HttpResponse response;
+        response.StatusCode = r.status_code;
+        response.Body = r.text;
+        response.ErrorMessage = r.error.message;
+        response.bSuccess = (r.status_code >= 200 && r.status_code < 300);
+
+        std::lock_guard<std::mutex> lock(m_Impl->queueMutex);
+        m_Impl->resultQueue.push_back({ std::move(response), std::move(callback) });
+
+        }).detach();
+}
---
+++ Engine/EngineCore/Systems/Public/HttpManager.h
@@ -0,0 +1,38 @@
+﻿#pragma once
+#include <string>
+#include <functional>
+
+// 通信結果を格納する構造体
+struct HttpResponse {
+    long StatusCode = 0;           // 200, 404, 500 など
+    std::string Body;              // サーバーからのレスポンス(JSON文字列など)
+    std::string ErrorMessage;      // 通信エラー時のメッセージ
+    bool bSuccess = false;         // StatusCode が 200番台なら true
+};
+
+// リクエスト完了時に呼び出されるコールバックの型
+using HttpCallback = std::function<void(const HttpResponse&)>;
+
+class HttpManager {
+public:
+    static HttpManager& GetInstance();
+
+    // 毎フレーム呼び出して、完了した通信のコールバックを実行する
+    void Update();
+
+    // HTTP GETリクエスト
+    void Get(const std::string& url, HttpCallback callback);
+
+    // HTTP POSTリクエスト (JSON送信)
+    void PostJson(const std::string& url, const std::string& jsonBody, HttpCallback callback);
+
+private:
+    HttpManager();
+    ~HttpManager();
+    HttpManager(const HttpManager&) = delete;
+    HttpManager& operator=(const HttpManager&) = delete;
+
+    // 実装を隠蔽するためのポインタ
+    struct Impl;
+    Impl* m_Impl = nullptr;
+};
