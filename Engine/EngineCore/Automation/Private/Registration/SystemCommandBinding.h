019183a4446b343c9573366366cadb959710feb9
A6P3W <186453117+A6P3W@users.noreply.github.com>
2026-09-06 19:07:30 +0900

    Automation Bindingの重複を解消

--- Engine/EngineCore/Automation/Private/Registration/SystemCommandBinding.h
+++ Engine/EngineCore/Automation/Private/Registration/SystemCommandBinding.h
@@ -2,25 +2,16 @@
 
 #include <functional>
 #include <nlohmann/json.hpp>
+#include <optional>
 #include <stdexcept>
 #include <string>
 #include <tuple>
 #include <type_traits>
 #include <utility>
 
-#include "Actor.h"
-#include "ActorComponent.h"
-#include "Registry/ActorMethodRegistry.h"
-#include "Registry/ComponentMethodRegistry.h"
-#include "Registry/SystemCommandRegistry.h"
 #include "Detail/AutomationMethodBinding.h"
 #include "Log.h"
-
-template <class T>
-using TAutomationArgumentStorage = std::remove_cvref_t<T>;
-
-template <class T>
-struct TAutomationMethodTraits;
+#include "Registry/SystemCommandRegistry.h"
 
 template <class T, class = void>
 struct TAutomationSystemCommandTraits;
@@ -57,24 +48,6 @@ template <class T>
 struct TAutomationSystemCommandTraits<T, std::void_t<decltype(&std::remove_cvref_t<T>::operator())>>
     : TAutomationSystemCommandTraits<decltype(&std::remove_cvref_t<T>::operator())> {};
 
-template <class TReturn, class TActor, class... TArgs>
-struct TAutomationMethodTraits<TReturn (TActor::*)(TArgs...)> {
-  using ActorType = TActor;
-  using ReturnType = TReturn;
-  using ArgumentTuple = std::tuple<TArgs...>;
-  static constexpr size_t ArgumentCount = sizeof...(TArgs);
-  static constexpr bool bIsConst = false;
-};
-
-template <class TReturn, class TActor, class... TArgs>
-struct TAutomationMethodTraits<TReturn (TActor::*)(TArgs...) const> {
-  using ActorType = TActor;
-  using ReturnType = TReturn;
-  using ArgumentTuple = std::tuple<TArgs...>;
-  static constexpr size_t ArgumentCount = sizeof...(TArgs);
-  static constexpr bool bIsConst = true;
-};
-
 template <class T>
 struct TAutomationParameter {
   using ValueType = T;
@@ -106,7 +79,7 @@ struct TIsOptional<std::optional<T>> : std::true_type {};
 
 template <class TParameter, class TArgument>
 constexpr bool bParameterMatchesArgument =
-    std::is_same_v<typename TParameter::ValueType, TAutomationArgumentStorage<TArgument>>;
+    std::is_same_v<typename TParameter::ValueType, std::remove_cvref_t<TArgument>>;
 
 template <class TParameter>
 nlohmann::json MakeParameterSchema(const TParameter& Parameter) {
@@ -162,88 +135,13 @@ auto ReadArguments(
     const TParametersTuple& Parameters,
     std::index_sequence<TIndices...>
 ) {
-  return std::tuple<TAutomationArgumentStorage<
+  return std::tuple<std::remove_cvref_t<
       std::tuple_element_t<TIndices, typename TTraits::ArgumentTuple>>...>{
-      ReadArgument<TAutomationArgumentStorage<
+      ReadArgument<std::remove_cvref_t<
           std::tuple_element_t<TIndices, typename TTraits::ArgumentTuple>>>(
           Arguments, std::get<TIndices>(Parameters)
       )...
   };
-}
-
-template <class TMethod, class TParametersTuple, size_t... TIndices>
-consteval bool ParametersMatch(std::index_sequence<TIndices...>) {
-  using TTraits = TAutomationMethodTraits<TMethod>;
-  return (
-      bParameterMatchesArgument<
-          std::tuple_element_t<TIndices, TParametersTuple>,
-          std::tuple_element_t<TIndices, typename TTraits::ArgumentTuple>> &&
-      ...
-  );
-}
-
-template <class TReturn>
-nlohmann::json ConvertReturnValue(TReturn&& Result) {
-  return TAutomationJsonConverter<std::remove_cvref_t<TReturn>>::ToJson(
-      std::forward<TReturn>(Result)
-  );
-}
-
-template <class TObject, class TMethod, class TParametersTuple>
-nlohmann::json InvokeRegisteredMethod(
-    TObject& Object,
-    TMethod Method,
-    const nlohmann::json& Arguments,
-    const TParametersTuple& Parameters
-) {
-  using TTraits = TAutomationMethodTraits<TMethod>;
-  using TReturn = typename TTraits::ReturnType;
-
-  auto Values = ReadArguments<TTraits>(
-      Arguments, Parameters, std::make_index_sequence<TTraits::ArgumentCount>{}
-  );
-  if constexpr (std::is_void_v<TReturn>) {
-    std::apply(
-        [&Object, Method](auto&&... MethodArguments) {
-          std::invoke(Method, Object, std::forward<decltype(MethodArguments)>(MethodArguments)...);
-        },
-        std::move(Values)
-    );
-    return nlohmann::json(nullptr);
-  } else {
-    decltype(auto) Result = std::apply(
-        [&Object, Method](auto&&... MethodArguments) -> decltype(auto) {
-          return std::invoke(
-              Method, Object, std::forward<decltype(MethodArguments)>(MethodArguments)...
-          );
-        },
-        std::move(Values)
-    );
-    return ConvertReturnValue(std::forward<decltype(Result)>(Result));
-  }
-}
-inline void RegisterDescriptor(
-    FAutomationActorMethodRegistry& Registry,
-    std::string ClassName,
-    FAutomationMethodDescriptor Descriptor
-) {
-  std::string Error;
-  if (!Registry.RegisterMethod(std::move(ClassName), std::move(Descriptor), &Error)) {
-    M_LOG(Error, "Automation method registration failed: {}", Error);
-    throw std::runtime_error(Error);
-  }
-}
-
-inline void RegisterComponentDescriptor(
-    FAutomationComponentMethodRegistry& Registry,
-    std::string ClassName,
-    FAutomationComponentMethodDescriptor Descriptor
-) {
-  std::string Error;
-  if (!Registry.RegisterMethod(std::move(ClassName), std::move(Descriptor), &Error)) {
-    M_LOG(Error, "Automation component method registration failed: {}", Error);
-    throw std::runtime_error(Error);
-  }
 }
 
 inline void RegisterSystemCommandDescriptor(
@@ -267,174 +165,6 @@ consteval bool SystemCommandParametersMatch(std::index_sequence<TIndices...>) {
   );
 }
 }  // namespace Detail
-
-template <class TMethod, class... TParameters>
-void RegisterMethod(
-    FAutomationActorMethodRegistry& Registry,
-    std::string MethodName,
-    std::string Description,
-    EAutomationPermission Permission,
-    TMethod Method,
-    TParameters... Parameters
-) {
-  using TTraits = TAutomationMethodTraits<TMethod>;
-  using TActor = typename TTraits::ActorType;
-  using TReturn = typename TTraits::ReturnType;
-  using TParametersTuple = std::tuple<TParameters...>;
-  static_assert(std::is_base_of_v<AActor, TActor>, "Automation methods must belong to an Actor.");
-  static_assert(
-      TTraits::ArgumentCount == sizeof...(TParameters),
-      "Every automation method argument requires one parameter definition."
-  );
-  static_assert(
-      Detail::ParametersMatch<TMethod, TParametersTuple>(
-          std::make_index_sequence<sizeof...(TParameters)>{}
-      ),
-      "Automation parameter types must match the method argument types."
-  );
-
-  TParametersTuple ParameterDefinitions(std::move(Parameters)...);
-  FAutomationMethodDescriptor Descriptor;
-  Descriptor.Name = std::move(MethodName);
-  Descriptor.Description = std::move(Description);
-  Descriptor.Permission = Permission;
-  Descriptor.InputSchema = std::apply(
-      [](const auto&... Values) { return Detail::MakeInputSchema(Values...); }, ParameterDefinitions
-  );
-  Descriptor.Handler = [Method, ParameterDefinitions = std::move(ParameterDefinitions)](
-                           AActor& Actor, const nlohmann::json& Arguments
-                       ) {
-    auto* TypedActor = dynamic_cast<TActor*>(&Actor);
-    if (!TypedActor) {
-      throw std::runtime_error("Automation actor type mismatch.");
-    }
-    return Detail::InvokeRegisteredMethod(*TypedActor, Method, Arguments, ParameterDefinitions);
-  };
-
-  Detail::RegisterDescriptor(Registry, TActor::StaticClassName(), std::move(Descriptor));
-}
-
-template <class TMethod, class... TParameters>
-void RegisterComponentMethod(
-    FAutomationComponentMethodRegistry& Registry,
-    std::string MethodName,
-    std::string Description,
-    EAutomationPermission Permission,
-    TMethod Method,
-    TParameters... Parameters
-) {
-  using TTraits = TAutomationMethodTraits<TMethod>;
-  using TComponent = typename TTraits::ActorType;
-  using TParametersTuple = std::tuple<TParameters...>;
-  static_assert(
-      std::is_base_of_v<MActorComponent, TComponent>,
-      "Automation component methods must belong to an MActorComponent."
-  );
-  static_assert(
-      TTraits::ArgumentCount == sizeof...(TParameters),
-      "Every automation component method argument requires one parameter definition."
-  );
-  static_assert(
-      Detail::ParametersMatch<TMethod, TParametersTuple>(
-          std::make_index_sequence<sizeof...(TParameters)>{}
-      ),
-      "Automation parameter types must match the method argument types."
-  );
-
-  TParametersTuple ParameterDefinitions(std::move(Parameters)...);
-  FAutomationComponentMethodDescriptor Descriptor;
-  Descriptor.Name = std::move(MethodName);
-  Descriptor.Description = std::move(Description);
-  Descriptor.Permission = Permission;
-  Descriptor.InputSchema = std::apply(
-      [](const auto&... Values) { return Detail::MakeInputSchema(Values...); }, ParameterDefinitions
-  );
-  Descriptor.Handler = [Method, ParameterDefinitions = std::move(ParameterDefinitions)](
-                           MActorComponent& Component, const nlohmann::json& Arguments
-                       ) {
-    auto* TypedComponent = dynamic_cast<TComponent*>(&Component);
-    if (!TypedComponent) {
-      throw std::runtime_error("Automation component type mismatch.");
-    }
-    return Detail::InvokeRegisteredMethod(*TypedComponent, Method, Arguments, ParameterDefinitions);
-  };
-
-  Detail::RegisterComponentDescriptor(
-      Registry, TComponent::StaticComponentClassName(), std::move(Descriptor)
-  );
-}
-
-template <class TMethod, class TResultAdapter, class... TParameters>
-void RegisterMethodWithResultAdapter(
-    FAutomationActorMethodRegistry& Registry,
-    std::string MethodName,
-    std::string Description,
-    EAutomationPermission Permission,
-    TMethod Method,
-    TResultAdapter ResultAdapter,
-    TParameters... Parameters
-) {
-  using TTraits = TAutomationMethodTraits<TMethod>;
-  using TActor = typename TTraits::ActorType;
-  using TReturn = typename TTraits::ReturnType;
-  using TParametersTuple = std::tuple<TParameters...>;
-  static_assert(std::is_base_of_v<AActor, TActor>, "Automation methods must belong to an Actor.");
-  static_assert(
-      TTraits::ArgumentCount == sizeof...(TParameters),
-      "Every automation method argument requires one parameter definition."
-  );
-  static_assert(
-      Detail::ParametersMatch<TMethod, TParametersTuple>(
-          std::make_index_sequence<sizeof...(TParameters)>{}
-      ),
-      "Automation parameter types must match the method argument types."
-  );
-
-  TParametersTuple ParameterDefinitions(std::move(Parameters)...);
-  FAutomationMethodDescriptor Descriptor;
-  Descriptor.Name = std::move(MethodName);
-  Descriptor.Description = std::move(Description);
-  Descriptor.Permission = Permission;
-  Descriptor.InputSchema = std::apply(
-      [](const auto&... Values) { return Detail::MakeInputSchema(Values...); }, ParameterDefinitions
-  );
-  Descriptor.Handler = [Method,
-                        ResultAdapter = std::move(ResultAdapter),
-                        ParameterDefinitions = std::move(ParameterDefinitions)](
-                           AActor& Actor, const nlohmann::json& Arguments
-                       ) {
-    auto* TypedActor = dynamic_cast<TActor*>(&Actor);
-    if (!TypedActor) {
-      throw std::runtime_error("Automation actor type mismatch.");
-    }
-    auto Values = Detail::ReadArguments<TTraits>(
-        Arguments, ParameterDefinitions, std::make_index_sequence<TTraits::ArgumentCount>{}
-    );
-    if constexpr (std::is_void_v<TReturn>) {
-      std::apply(
-          [TypedActor, Method](auto&&... MethodArguments) {
-            std::invoke(
-                Method, *TypedActor, std::forward<decltype(MethodArguments)>(MethodArguments)...
-            );
-          },
-          std::move(Values)
-      );
-      return std::invoke(ResultAdapter, *TypedActor);
-    } else {
-      decltype(auto) Result = std::apply(
-          [TypedActor, Method](auto&&... MethodArguments) -> decltype(auto) {
-            return std::invoke(
-                Method, *TypedActor, std::forward<decltype(MethodArguments)>(MethodArguments)...
-            );
-          },
-          std::move(Values)
-      );
-      return std::invoke(ResultAdapter, *TypedActor, std::forward<decltype(Result)>(Result));
-    }
-  };
-
-  Detail::RegisterDescriptor(Registry, TActor::StaticClassName(), std::move(Descriptor));
-}
 
 template <class TCallable, class... TParameters>
 void RegisterSystemCommand(
