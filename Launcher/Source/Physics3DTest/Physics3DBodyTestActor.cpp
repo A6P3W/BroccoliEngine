436c002983223fbe3481db761820ec8624348d9d
A6P3W <186453117+A6P3W@users.noreply.github.com>
2026-09-20 14:46:11 +0900

    test(physics3d): expose overlap observations through control

--- Launcher/Source/Physics3DTest/Physics3DBodyTestActor.cpp
+++ Launcher/Source/Physics3DTest/Physics3DBodyTestActor.cpp
@@ -1,6 +1,9 @@
 #include "Physics3DBodyTestActor.h"
 
+#include <algorithm>
+
 #include "BoxCollisionComponent3D.h"
+#include "ControlMacros.h"
 #include "CubeComponent.h"
 #include "RigidBody3DComponent.h"
 #include "SphereCollisionComponent3D.h"
@@ -9,6 +12,32 @@ REGISTER_ACTOR(APhysics3DStaticFloorActor)
 REGISTER_ACTOR(APhysics3DDynamicBoxActor)
 REGISTER_ACTOR(APhysics3DDynamicSphereActor)
 REGISTER_ACTOR(APhysics3DKinematicActor)
+CONTROL_METHOD(
+    "get_collision_observation",
+    "Returns 3D overlap event counts and current overlap actor IDs.",
+    &APhysics3DDynamicBoxActor::GetCollisionObservation,
+    CONTROL_PARAMETERS(),
+    ([](const FPhysics3DCollisionObservation& Observation) {
+      return nlohmann::json{
+          {"begin_overlap_count", Observation.BeginOverlapCount},
+          {"end_overlap_count", Observation.EndOverlapCount},
+          {"overlapping_actor_ids", Observation.OverlappingActorIds}
+      };
+    })
+)
+CONTROL_METHOD(
+    "get_collision_observation",
+    "Returns 3D overlap event counts and current overlap actor IDs.",
+    &APhysics3DDynamicSphereActor::GetCollisionObservation,
+    CONTROL_PARAMETERS(),
+    ([](const FPhysics3DCollisionObservation& Observation) {
+      return nlohmann::json{
+          {"begin_overlap_count", Observation.BeginOverlapCount},
+          {"end_overlap_count", Observation.EndOverlapCount},
+          {"overlapping_actor_ids", Observation.OverlappingActorIds}
+      };
+    })
+)
 
 namespace {
 void ConfigureBody(AActor& Actor, ERigidBody3DType Type) {
@@ -46,6 +75,26 @@ APhysics3DDynamicBoxActor::APhysics3DDynamicBoxActor() {
 
 void APhysics3DDynamicBoxActor::BeginPlay() { SetActorLocation3D({-2.0f, 5.0f, 0.0f}); }
 
+FPhysics3DCollisionObservation APhysics3DDynamicBoxActor::GetCollisionObservation() const {
+  return {BeginOverlapCount, EndOverlapCount, OverlappingActorIds};
+}
+
+void APhysics3DDynamicBoxActor::BeginOverlap(AActor* OtherActor) {
+  if (!OtherActor) {
+    return;
+  }
+  ++BeginOverlapCount;
+  OverlappingActorIds.push_back(OtherActor->GetActorId());
+}
+
+void APhysics3DDynamicBoxActor::EndOverlap(AActor* OtherActor) {
+  if (!OtherActor) {
+    return;
+  }
+  ++EndOverlapCount;
+  std::erase(OverlappingActorIds, OtherActor->GetActorId());
+}
+
 APhysics3DDynamicSphereActor::APhysics3DDynamicSphereActor() {
   ConfigureBody(*this, ERigidBody3DType::Dynamic);
   MSphereCollisionComponent3D* Collider = NewObject<MSphereCollisionComponent3D>(this);
@@ -59,6 +108,26 @@ APhysics3DDynamicSphereActor::APhysics3DDynamicSphereActor() {
 }
 
 void APhysics3DDynamicSphereActor::BeginPlay() { SetActorLocation3D({2.0f, 5.0f, 0.0f}); }
+
+FPhysics3DCollisionObservation APhysics3DDynamicSphereActor::GetCollisionObservation() const {
+  return {BeginOverlapCount, EndOverlapCount, OverlappingActorIds};
+}
+
+void APhysics3DDynamicSphereActor::BeginOverlap(AActor* OtherActor) {
+  if (!OtherActor) {
+    return;
+  }
+  ++BeginOverlapCount;
+  OverlappingActorIds.push_back(OtherActor->GetActorId());
+}
+
+void APhysics3DDynamicSphereActor::EndOverlap(AActor* OtherActor) {
+  if (!OtherActor) {
+    return;
+  }
+  ++EndOverlapCount;
+  std::erase(OverlappingActorIds, OtherActor->GetActorId());
+}
 
 APhysics3DKinematicActor::APhysics3DKinematicActor() {
   ConfigureBody(*this, ERigidBody3DType::Kinematic);
--- Launcher/Source/Physics3DTest/Physics3DBodyTestActor.h
+++ Launcher/Source/Physics3DTest/Physics3DBodyTestActor.h
@@ -6,6 +6,12 @@ class MBoxCollisionComponent3D;
 class MRigidBody3DComponent;
 class MSphereCollisionComponent3D;
 
+struct FPhysics3DCollisionObservation {
+  uint32_t BeginOverlapCount = 0;
+  uint32_t EndOverlapCount = 0;
+  std::vector<FActorId> OverlappingActorIds;
+};
+
 class APhysics3DStaticFloorActor final : public AActor {
  public:
   DEFINE_ACTOR_CLASS(APhysics3DStaticFloorActor)
@@ -19,18 +25,34 @@ class APhysics3DDynamicBoxActor final : public AActor {
  public:
   DEFINE_ACTOR_CLASS(APhysics3DDynamicBoxActor)
   APhysics3DDynamicBoxActor();
+  FPhysics3DCollisionObservation GetCollisionObservation() const;
 
  protected:
   void BeginPlay() override;
+  void BeginOverlap(AActor* OtherActor) override;
+  void EndOverlap(AActor* OtherActor) override;
+
+ private:
+  uint32_t BeginOverlapCount = 0;
+  uint32_t EndOverlapCount = 0;
+  std::vector<FActorId> OverlappingActorIds;
 };
 
 class APhysics3DDynamicSphereActor final : public AActor {
  public:
   DEFINE_ACTOR_CLASS(APhysics3DDynamicSphereActor)
   APhysics3DDynamicSphereActor();
+  FPhysics3DCollisionObservation GetCollisionObservation() const;
 
  protected:
   void BeginPlay() override;
+  void BeginOverlap(AActor* OtherActor) override;
+  void EndOverlap(AActor* OtherActor) override;
+
+ private:
+  uint32_t BeginOverlapCount = 0;
+  uint32_t EndOverlapCount = 0;
+  std::vector<FActorId> OverlappingActorIds;
 };
 
 class APhysics3DKinematicActor final : public AActor {
